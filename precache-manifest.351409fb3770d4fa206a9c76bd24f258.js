68597a2ea23135911acf2d6805d094c1a14e45d2
Aplank14 <aplank14@gmail.com>
2020-08-26 15:06:41 -0400

    Updates

---
+++ precache-manifest.351409fb3770d4fa206a9c76bd24f258.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c165835d10ace9d2c4d17e51b50b8f56",
+    "url": "/aplank14.github.io/index.html"
+  },
+  {
+    "revision": "0a0e09c5c217b601f5dc",
+    "url": "/aplank14.github.io/static/css/main.2feae731.chunk.css"
+  },
+  {
+    "revision": "ea1f46f2e2a68bc2f9ae",
+    "url": "/aplank14.github.io/static/js/2.b00e8bf2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aplank14.github.io/static/js/2.b00e8bf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0e09c5c217b601f5dc",
+    "url": "/aplank14.github.io/static/js/main.3db00653.chunk.js"
+  },
+  {
+    "revision": "bf900c9fbfe73bf5cd14",
+    "url": "/aplank14.github.io/static/js/runtime-main.a078f5ec.js"
+  },
+  {
+    "revision": "35663db797489723d7f1dd4816f7b171",
+    "url": "/aplank14.github.io/static/media/Internship.35663db7.pdf"
+  },
+  {
+    "revision": "51d3b42c04f8f42ec11789a4e0ccfeda",
+    "url": "/aplank14.github.io/static/media/landing.51d3b42c.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/aplank14.github.io/precache-manifest.351409fb3770d4fa206a9c76bd24f258.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aplank14.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
